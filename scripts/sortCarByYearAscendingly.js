9c82b94225c6feac56bd021940d15dba832740c1
andragil <andiragilw09@gmail.com>
2024-03-09 08:18:55 +0700

    second commit

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,12 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  result.sort((a, b) => a.year - b.year);
+
+  return result;
+}
